1b1d558f73f8da77a501cc2bad331862a9863eea
tm <torrez.mn@gmail.com>
2023-12-11 22:36:32 -0300

    UPDATED - {2023-12-11} - {22:36:32}

--- main.py
+++ main.py
@@ -12,21 +12,33 @@ from scripts.infobae import infobae_main
 if __name__ == "__main__":
 
     try:
+        print('='*44)
+        print("Running EL CRONISTA.")
+        print('='*44)
         el_cronista_main()
     except Exception as e:
         print("El Cronista", e)
 
     try:
+        print('='*44)
+        print("Running LA NACION.")
+        print('='*44)
         la_nacion_main()
     except Exception as e:
         print("La Nacion", e)
 
     try:
+        print('='*44)
+        print("Running AMBITO.")
+        print('='*44)
         ambito_main()
     except Exception as e:
         print("Ambito", e)
 
     try:
+        print('='*44)
+        print("Running INFOBAE.")
+        print('='*44)
         infobae_main()
     except Exception as e:
         print("Infobae", e)
